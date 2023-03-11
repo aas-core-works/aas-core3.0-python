cbb01646d21ba0dc2a5fed2a8f3dfd30c256f605
Marko Ristin <marko.ristin@gmail.com>
2023-03-11 18:50:19 +0100

    update to aas-core-meta 97dd12a

--- aas_core3/verification.py
+++ aas_core3/verification.py
@@ -2483,24 +2483,20 @@ class _Transformer(aas_types.AbstractTransformer[Iterator[Error]]):
         if not (
             not (that.submodel_elements is not None)
             or (
-                not (that.kind == aas_types.ModellingKind.TEMPLATE)
-                or (
-                    not (
-                        any(
-                            not (submodel_element.qualifiers is not None)
-                            or (
-                                not (
-                                    any(
-                                        qualifier.kind_or_default()
-                                        == aas_types.QualifierKind.TEMPLATE_QUALIFIER
-                                        for qualifier in submodel_element.qualifiers
-                                    )
-                                )
+                not (
+                    any(
+                        not (submodel_element.qualifiers is not None)
+                        or (
+                            any(
+                                qualifier.kind_or_default()
+                                == aas_types.QualifierKind.TEMPLATE_QUALIFIER
+                                for qualifier in submodel_element.qualifiers
                             )
-                            for submodel_element in that.submodel_elements
                         )
+                        for submodel_element in that.submodel_elements
                     )
                 )
+                or (that.kind == aas_types.ModellingKind.TEMPLATE)
             )
         ):
             yield Error(
