202484d02d39f2f7e6defb824068f12dbbdd4efc
Marko Ristin <marko@ristin.ch>
2025-05-13 11:18:12 +0500

    Update to aas-core-meta, codegen, testgen f2fd738, c77b7dc6, a8c4155e4 (#50)
    We update the development requirements to and re-generate everything
    with:
    * [aas-core-meta f2fd738],
    * [aas-core-codegen c77b7dc6] and
    * [aas-core3.0-testgen a8c4155e4].
    
    Notably, we propagate all the fixes for V3.0.2.
    
    [aas-core-meta f2fd738]: https://github.com/aas-core-works/aas-core-meta/commit/f2fd738
    [aas-core-codegen c77b7dc6]: https://github.com/aas-core-works/aas-core-codegen/commit/c77b7dc6
    [aas-core3.0-testgen a8c4155e4]: https://github.com/aas-core-works/aas-core3.0-testgen/commit/a8c4155e4

--- aas_core3/verification.py
+++ aas_core3/verification.py
@@ -5808,6 +5808,16 @@ def verify(that: aas_types.Class) -> Iterator[Error]:
     yield from _TRANSFORMER.transform(that)
 
 
+def verify_xml_serializable_string(that: str) -> Iterator[Error]:
+    """Verify the constraints of :paramref:`that`."""
+    if not matches_xml_serializable_string(that):
+        yield Error(
+            "Constraint AASd-130: An attribute with data type 'string' "
+            + "shall consist of these characters only: "
+            + "^[\\x09\\x0A\\x0D\\x20-\\uD7FF\\uE000-\\uFFFD\\U00010000-\\U0010FFFF]*$."
+        )
+
+
 def verify_non_empty_xml_serializable_string(that: str) -> Iterator[Error]:
     """Verify the constraints of :paramref:`that`."""
     if not matches_xml_serializable_string(that):
@@ -6038,16 +6048,14 @@ def verify_qualifier_type(that: str) -> Iterator[Error]:
         yield Error("Name type shall have a maximum length of 128 characters.")
 
 
-# noinspection PyUnusedLocal
 def verify_value_data_type(that: str) -> Iterator[Error]:
     """Verify the constraints of :paramref:`that`."""
-    # There is no verification specified.
-    return
-
-    # Empty generator according to:
-    # https://stackoverflow.com/a/13243870/1600678
-    # noinspection PyUnreachableCode
-    yield
+    if not matches_xml_serializable_string(that):
+        yield Error(
+            "Constraint AASd-130: An attribute with data type 'string' "
+            + "shall consist of these characters only: "
+            + "^[\\x09\\x0A\\x0D\\x20-\\uD7FF\\uE000-\\uFFFD\\U00010000-\\U0010FFFF]*$."
+        )
 
 
 def verify_id_short_type(that: str) -> Iterator[Error]:
